edce592c8f50303ea852f8cd66595f91bc7eea47
newisdartagnan <newisboris@gmail.com>
2023-03-02 19:13:53 -0800

    update try

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -6,33 +6,42 @@
  * @root: pointer to the root node of the tree
  * @value: value to remove in the tree
  *
- * Return: pointer to the new root node of the tree after removing the desired value
+ * Return: pointer to the new root node of the tree after
+ * removing the desired value
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
 	if (root == NULL)
-		return NULL;
+		return (NULL);
 
 	if (value < root->n)
 		root->left = bst_remove(root->left, value);
 	else if (value > root->n)
 		root->right = bst_remove(root->right, value);
-	else {
-		if (root->left == NULL && root->right == NULL) {
+	else
+	{
+		if (root->left == NULL && root->right == NULL)
+		{
 			free(root);
 			root = NULL;
-		} else if (root->left == NULL) {
+		} else if (root->left == NULL)
+		{
 			bst_t *temp = root;
+
 			root = root->right;
 			root->parent = temp->parent;
 			free(temp);
-		} else if (root->right == NULL) {
+		} else if (root->right == NULL)
+		{
 			bst_t *temp = root;
+
 			root = root->left;
 			root->parent = temp->parent;
 			free(temp);
-		} else {
+		} else
+		{
 			bst_t *temp = root->right;
+
 			while (temp->left != NULL)
 				temp = temp->left;
 			root->n = temp->n;
@@ -40,6 +49,6 @@ bst_t *bst_remove(bst_t *root, int value)
 		}
 	}
 
-	return root;
+	return (root);
 }
 
