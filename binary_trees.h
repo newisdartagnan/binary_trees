b0f7c34b239c69f15d9d9ed95f7e2f57487257e4
newisdartagnan <newisboris@gmail.com>
2023-03-02 18:52:01 -0800

    try

--- binary_trees.h
+++ binary_trees.h
@@ -55,5 +55,5 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 int binary_tree_is_complete(const binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
 bst_t *bst_insert(bst_t **tree, int value);
-
+bst_t *array_to_bst(int *array, size_t size);
 #endif /* BINARY_TREES_H */
